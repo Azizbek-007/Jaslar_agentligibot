b8d887e558b65704241464e75371b7afd965571f
Azizbek-007 <77209269+Azizbek-007@users.noreply.github.com>
2022-09-01 00:09:26 +0500

    commit

--- handler/user/message.py
+++ handler/user/message.py
@@ -13,3 +13,5 @@ async def send_answer(message: types.Message):
     await message.forward(chat_id)
     await bot.send_message(chat_id, text=f"👆⏳{time}\n\n```{message.from_user.id}```", parse_mode='markdown')
     await message.reply("Sizdiń juwabıńız qabıllandı ✔️")
+ 
+ 
