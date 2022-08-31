de3c1cdca5dca2e1363f75c1d60b8eec176f9255
Azizbek-007 <77209269+Azizbek-007@users.noreply.github.com>
2022-09-01 00:28:59 +0500

    any

--- handler/user/message.py
+++ handler/user/message.py
@@ -1,17 +1,17 @@
 from aiogram import types
 from filters.join_check import  IsMember
 from loader import dp, bot
-import datetime
-import pytz
+from datetime import datetime
+from pytz import timezone
 from data.config import chat_id
 
 @dp.message_handler(IsMember(), content_types=['text'], chat_type=types.ChatType.PRIVATE)
 async def send_answer(message: types.Message):
-    tz = pytz.timezone("Asia/Tashkent")
-    now = tz.localize(datetime.datetime.now())
-    time = now.strftime('%Y-%m-%d %H:%M:%S')
+    tz = timezone("Asia/Tashkent")
+    dt_format = "%Y-%m-%d %H:%M:%S"
+    moscowZone = datetime.now(tz)
+    time = moscowZone.strftime(dt_format)
     await message.forward(chat_id)
     await bot.send_message(chat_id, text=f"👆⏳{time}\n\n```{message.from_user.id}```", parse_mode='markdown')
     await message.reply("Sizdiń juwabıńız qabıllandı ✔️")
- 
  
